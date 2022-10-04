bc1df26eba13a8849260a2109f50989424ae1b80
Ирина <mi.irene97@gmail.com>
2022-10-04 21:39:28 +0300

    Доработанная домашка

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,9 @@ public class Main {
 
         System.out.println("Задание 3");
         int deliveryDistance = 95;
-        int minDeliveryDays = 1;
-        int firstInterval = 20;
-        int interval = 40;
-        int totalDeliveryDays = calculateDeliveryDays(deliveryDistance, minDeliveryDays, firstInterval, interval);
-        StringBuilder daysForDisplay = new StringBuilder();
-        if (totalDeliveryDays == 1) {
-            daysForDisplay.append("день");
-        } else if (totalDeliveryDays >= 2 && totalDeliveryDays <= 4) {
-            daysForDisplay.append("дня");
-        } else {
-            daysForDisplay.append("дней");
-        }
-        System.out.println("На доставку потребуется " + totalDeliveryDays + " " + daysForDisplay + ".");
+        int totalDeliveryDays = calculateDeliveryDays(deliveryDistance);
+        StringBuilder daysForDisplay = modifyWordDay(totalDeliveryDays);
+        System.out.printf("На доставку потребуется %s %s.%n", totalDeliveryDays, daysForDisplay);
     }
     public static void checkLeapYear (int year) {
         if (year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
@@ -36,31 +26,49 @@ public class Main {
     }
 
     public static void recommendApp(int OS, int deviceYear) {
-        if (OS == 0) {
-            if (deviceYear == LocalDate.now().getYear()) {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-            } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            }
+        if (OS != 0 && OS != 1) {
+            throw new IllegalArgumentException("Недопустимый тип операционной системы.");
+        }
+        if (deviceYear > LocalDate.now().getYear()) {
+            throw new IllegalArgumentException("Недопустимый год выпуска устройства.");
+        }
+        String osString = OS == 0 ? "iOS" : "Android";
+        String versionString = deviceYear == LocalDate.now().getYear() ? "полную" : "облегченную";
+        System.out.printf("Установите %s версию приложения для %s по ссылке.%n", versionString, osString);
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1;
         } else {
-            if (deviceYear == LocalDate.now().getYear()) {
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            }
+            return (int) Math.round((double) deliveryDistance / 40) + 1;
         }
     }
 
-    public static int calculateDeliveryDays(int deliveryDistance, int minDeliveryDays, int firstInterval, int interval) {
-        int totalDeliveryDays;
-        if (deliveryDistance <= firstInterval) {
-            return minDeliveryDays;
-        } else if ((deliveryDistance - firstInterval) % interval == 0) {
-            totalDeliveryDays = minDeliveryDays + (deliveryDistance - firstInterval) / interval;
-            return totalDeliveryDays;
-        } else {
-            totalDeliveryDays = minDeliveryDays + 1 + (deliveryDistance - firstInterval) / interval;
-            return totalDeliveryDays;
+    public static StringBuilder modifyWordDay(int numberOfDays) {
+        StringBuilder daysForDisplay = new StringBuilder();
+        if (numberOfDays >= 11 && numberOfDays <= 14) {
+            daysForDisplay.append("дней");
+            return daysForDisplay;
         }
+        String days = Integer.toString(numberOfDays);
+        switch (days.charAt(days.length() - 1)) {
+            case '1':
+                daysForDisplay.append("день");
+                break;
+            case '2':
+            case '3':
+            case '4':
+                daysForDisplay.append("дня");
+                break;
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                daysForDisplay.append("дней");
+        }
+        return daysForDisplay;
     }
 }
